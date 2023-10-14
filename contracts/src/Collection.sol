d01040806f111f2297ddc8d7c7f3cd1a7493cb9d
Lina AZEROUK <azerouklyna2001@hotmail.com>
2023-10-14 20:03:33 +0200

    ownable

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
+import "./Ownable.sol";
 
-contract Collection {
+
+contract Collection  is Ownable{
   string public name;
   int public cardCount;
+  mapping (address => uint) ownerCollectionCount;
 
   constructor(string memory _name, int _cardCount) {
     name = _name;
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -15,10 +15,22 @@ contract Main  is ERC721 {
   function createCollection(string calldata name, int cardCount) external {
     collections[count++] = new Collection(name, cardCount);
   }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    return zombieToOwner[_tokenId];
+  }
   // @notice Explain to an end user what this does
     /// @dev Explain to a developer any extra details
     /// @param Documents a parameter just like in doxygen (must be followed by parameter name);
-   function ownerOf(int cardCount) external view returns (string) {
-    return collections[cardCount].name;
+   function ownerOf(int id) external view returns (string) {
+    return collections[id].name;
   }
+
+
+    function getcollectionByOwner(address _owner) external view returns(uint[] memory) {
+          uint[] memory result = new uint[](ownerCollectionCount[_owner]);
+          result[0] = 0;  //needs to be changedd
+          return result;
+
+    }
 }
