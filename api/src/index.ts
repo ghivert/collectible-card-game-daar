ed9c53ddd9457f6f66702fa79260638a5e01ae6c
Lina AZEROUK <azerouklyna2001@hotmail.com>
2023-10-16 16:41:18 +0200

    yes API contacte the contracte

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,22 +1,31 @@
 // index.ts
 import express from 'express';
-import Web3 from 'web3';
 import MAIN_ABI from './config/Main_abi';
-const web3 = new Web3('https://ropsten.infura.io/v3/your_infura_project_id');
+const {Web3} = require('web3');
+
+const provider = new Web3.providers.HttpProvider("http://localhost:8545")
+const web3 = new Web3(provider);
+
+//const web3 = new Web3('http://127.0.0.1:8545/');
+
+//const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545/'));
+
 const CONTRACT_ADDRESSS = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
 const abi = MAIN_ABI;
 const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESSS);
 
 const app = express();
 const port = 8000;
-app.use(express.json());
+app.use(express.json());  
 
 
 app.get("/", async (req, res) => {
   try {
-    const user_adress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-    const contractData = await contract.methods.create_collection_then_assign_to_user().call();
-    res.json({ data: contractData });
+   const user_adress = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+    //const contractData = await contract.methods.balanceOf(user_adress).call();
+    //res.json({ data: contractData });
+    web3.eth.getBalance(user_adress).then(console.log);
+
   } catch (error) {
     res.status(500).json({ error: 'Erreur lors de l\'appel du contrat' });
   }
