3bfdd7dfdaefb96150443855e075bc9aa699c135
Lina AZEROUK <azerouklyna2001@hotmail.com>
2023-10-16 16:07:05 +0200

    api update

---
+++ api/src/config/Main_abi.ts
@@ -0,0 +1,108 @@
+const MAIN_ABI = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "admin",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "int256"
+            }
+        ],
+        "name": "collections",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "count",
+        "outputs": [
+            {
+                "name": "",
+                "type": "int256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "name": "cardCount",
+                "type": "int256"
+            }
+        ],
+        "name": "createCollection",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "user_adress",
+                "type": "address"
+            },
+            {
+                "name": "collection_adresse",
+                "type": "address"
+            }
+        ],
+        "name": "assign_cardPokemon",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "user_adress",
+                "type": "address"
+            }
+        ],
+        "name": "create_collection_then_assign_to_user",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    }
+]
+
+
+export default MAIN_ABI
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,21 +1,34 @@
 // index.ts
 import express from 'express';
+import Web3 from 'web3';
+import MAIN_ABI from './config/Main_abi';
+const web3 = new Web3('https://ropsten.infura.io/v3/your_infura_project_id');
+const CONTRACT_ADDRESSS = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+const abi = MAIN_ABI;
+const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESSS);
+
 const app = express();
 const port = 8000;
-
 app.use(express.json());
 
+
+app.get("/", async (req, res) => {
+  try {
+    const user_adress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+    const contractData = await contract.methods.create_collection_then_assign_to_user().call();
+    res.json({ data: contractData });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de l\'appel du contrat' });
+  }
+  res.send("Hello world");
+});
+
+
 app.post('/mint', async (req, res) => {
-    const { address, tokenId } = req.body;
-
-    // Call the mint function on the smart contract using ethers.js
-    // Example code: (Assuming you've deployed the contract and have an ethers provider)
-    // const contract = new ethers.Contract(contractAddress, abi, signer);
-    // await contract.mint(address, tokenId);
-
-    res.send(`Minted NFT with tokenId: ${tokenId} to address: ${address}`);
+  const { address, tokenId } = req.body;
+  res.send(`Minted NFT with tokenId: ${tokenId} to address: ${address}`);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
