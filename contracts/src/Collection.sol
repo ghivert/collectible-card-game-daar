c847b8c90aa521f7bc84a049b2e2201b572ae3b8
Lina AZEROUK <azerouklyna2001@hotmail.com>
2023-10-16 11:59:42 +0200

    erc721

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 //pragma solidity  >=0.5.0 <0.6.0;
 pragma solidity ^0.8.16;
-import "./Ownable.sol";
 
 
-contract Collection  is Ownable{
+contract Collection {
   string public name;
   int public cardCount;
   mapping (address => uint) ownerCollectionCount;
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -2,9 +2,8 @@
 //pragma solidity >=0.5.0 <0.6.0;
 pragma solidity ^0.8.16;
 import "./Collection.sol";
-import "./ERC721.sol";
 
-contract Main  is ERC721 {
+contract Main   {
   int private count;
   mapping(int => Collection) public collections;
 
@@ -16,18 +15,8 @@ contract Main  is ERC721 {
     collections[count++] = new Collection(name, cardCount);
   }
 
-  function ownerOf(uint256 _tokenId) external view returns (address) {
-    return collections[_tokenId];
-  }
-   function ownerOf(int id) external view returns (string memory) {
-    return collections[id].name;
-  }
 
 
-    function getcollectionByOwner(address _owner) external view returns(uint[] memory) {
-          uint[] memory result = new uint[](0);
-          result[0] = 0;  //needs to be changedd
-          return result;
 
-    }
+
 }
