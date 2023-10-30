4f6311965ee3aac521fee34ae6040b5a54cabb17
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2023-10-30 19:33:51 +0100

    warmingup profil

--- frontend/src/components/user/User.component.tsx
+++ frontend/src/components/user/User.component.tsx
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from 'react'
 
 const User = props => {
+  const [userPokemons, setUserPokemons] = useState([]) //["id1", "id2", "id3]
   const [name, setName] = useState('')
 
   useEffect(() => {
-    const r = props.wallet?.details?.account
-    setName(r)
-  })
+    const user_adress = props.wallet?.details?.account
+    setName(user_adress)
+    props.wallet?.contract.allCardsUser(user_adress).then(data => {
+      console.log(data)
+      setUserPokemons(data)
+    })
+  }, [])
+
   return (
     <div>
       <h1>Welcome {name}</h1>
+      <h2>Your Pokemons</h2>
+      <div>
+        {userPokemons.map((pokemon, index) => (
+          <div key={index}>
+            <h3>Single pokemon</h3>
+            <p>{pokemon}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
