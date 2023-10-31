f6de394eb88590fa08c799355c2672ba430f5a6a
ubombar <ufukbombar@gmail.com>
2023-10-31 12:24:24 +0100

    Add nodemon to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,25 @@
-console.log("test")
+import express from 'express';
+
+import routes from './routes';
+
+class App {
+  public server;
+
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+console.log("Expressjs is running at {http://localhost:5657/}")
+new App().server.listen(5657);
