bb359e2bc76b081b9e4050d97c4c2e159109dcc5
RootLeo00 <caterina.leonelli@gmail.com>
2023-11-01 10:53:48 +0100

    first two solidity assignments. Working on 3rd one

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -1,12 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+//pragma solidity  >=0.5.0 <0.6.0;
+pragma solidity ^0.8.19;
 
-contract Collection {
-  string public name;
-  int public cardCount;
+import "./Ownable.sol";
+import "./ERC721.sol";
+import "./Ownable.sol";
 
-  constructor(string memory _name, int _cardCount) {
-    name = _name;
-    cardCount = _cardCount;
+contract Collection is Ownable {
+  string public collectionName; //it's kinda the id of the collection
+  uint256 public cardNumberMax; // number of cards in the collection (size)
+  uint256 public cardNumberMinted;  // number of cards created -1, because it functions as an index that starts from 0
+  mapping(uint256 => uint256) public collectionToCards; //given by indexes
+
+  constructor(string memory _collectionName, uint256 _cardNumberMax) {
+    collectionName = _collectionName;
+    cardNumberMax = _cardNumberMax;
+    cardNumberMinted = 0;
   }
+
+    //The owner will be able to mint and assign an arbitrary amount of cards to a user from a specified collection.
+  function mint(
+    uint256 _cardId
+    //string memory _tokenURI
+  ) public returns (bool) {
+    collectionToCards[cardNumberMinted] = _cardId;
+    cardNumberMinted++;
+    // emit Transfer(msg.sender, _to, _cardId);
+    return true;
+  }
+
+  function getCardIdByIndex(uint256 _index) public view returns (uint256) {
+    require(_index >= 0 && _index < cardNumberMinted, "Invalid index");
+    return collectionToCards[_index];
+  }
+
 }
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -1,17 +1,68 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.19;
 
 import "./Collection.sol";
+import "./Ownable.sol";
 
-contract Main {
-  int private count;
-  mapping(int => Collection) private collections;
+contract Main is Ownable {
+  uint256 private count;
+  mapping(uint256 => Collection) private collections;
+  mapping(uint256 => address) private collectionOwners;
+  mapping(address => Collection[]) private ownerCollections;
 
   constructor() {
     count = 0;
+    isAdmin[msg.sender] = true;
   }
 
-  function createCollection(string calldata name, int cardCount) external {
-    collections[count++] = new Collection(name, cardCount);
+  function createCollection(
+    string memory _collectionName
+  ) external returns (Collection) {
+    Collection collection = new Collection(_collectionName, 0);
+    collections[count] = collection;
+    count++;
+    return collection;
+  }
+
+    //The Main contract will have an owner (i.e. a super-admin), which will be able to mint and assign cards to a selected user in a selected collection.
+  function mintAndAssignCard(
+    uint256 _collectionId,
+    uint256 _cardId
+  ) internal onlyAdmin {
+    collections[_collectionId].mint(_cardId);
+  }
+
+  function mintAndAssignCards(
+    uint256 _collectionId,
+    uint256[] memory _cardIds
+  ) external onlyAdmin{
+    for (uint256 i = 0; i < _cardIds.length; i++) {
+      mintAndAssignCard(_collectionId, _cardIds[i]);
+    }
+  }
+
+  function getAllCollections() public view returns (string[] memory) {
+    Collection[] memory _collections = new Collection[](uint256(count));
+    string[] memory collectionNames = new string[](uint256(count));
+    uint256 index = 0;
+    for (uint256 i = 0; i < count; i++) {
+      _collections[index] = collections[i];
+      collectionNames[index] = collections[i].collectionName();
+      index++;
+    }
+    return collectionNames;
+  }
+
+  function getCardsFromCollection(
+    uint256 collectionName
+  ) public view returns (uint256[] memory) {
+    Collection collection = collections[collectionName];
+    uint256[] memory result = new uint256[](uint256(collection.cardNumberMinted()));
+    uint256 index = 0;
+    for (uint256 i = 0; i < collection.cardNumberMinted(); i++) { //.cardNumberMinted() retrieves the property "cardNumberMinted" from Collection contract 
+      result[index] = collection.getCardIdByIndex(i);
+      index++;
+    }
+    return result;
   }
 }
