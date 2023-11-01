5b4fb92df9b3853587236ecd5e9c1402063045e7
ubombar <ufukbombar@gmail.com>
2023-11-01 10:52:37 +0100

    Add cors function to backend

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -4,6 +4,7 @@ import * as ethereum from '@/lib/ethereum'
 import * as main from '@/lib/main'
 import { useNavigate } from 'react-router-dom';
 import { checkAccount } from '@/utilities'
+import axios from 'axios'
 
 export const AdminPage = () => {
   const [number, setNumber] = useState<number>(0)
@@ -13,7 +14,11 @@ export const AdminPage = () => {
   checkAccount(navigate)
 
   function buttonClick() {
-    console.log("1 click")
+    // console.log("1 click")
+    axios.defaults.headers[""]
+    axios.get("http://localhost:5657", {}).then((r) => {
+      console.log(r.data)
+    })
     setNumber(number+1)
   }
   return (
